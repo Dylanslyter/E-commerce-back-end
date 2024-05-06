5d354a1099a55703b9b6ac63114d64e2826a57d0
dylan <dylan.slyter@yahoo.com>
2024-05-05 20:20:13 -0400

    changing my models files and checking if they are correct

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,28 +1,3 @@
-const { Model, DataTypes } = require('sequelize');
-
-const sequelize = require('../config/connection');
-
-class ProductTag extends Model {}
-
-ProductTag.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
-  }
-);
-
-module.exports = ProductTag;
-
-
-
-
-
 
 
 // Import important parts of sequelize library
